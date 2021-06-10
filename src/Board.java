be80bf4652907c4cc72f606b6ef50da920db1bed
dev-raj-1729 <dev.raj.r1729@gmail.com>
2021-06-10 15:20:53 +0530

    added Board.java

--- src/Board.java
+++ src/Board.java
@@ -5,13 +5,13 @@ import javax.swing.*;
 public class Board extends JPanel implements ActionListener {
     private final int BOARD_WIDTH = 300;
     private final int BOARD_HEIGHT = 300;
-    private final int DOTS = 900;
+    private final int MAX_DOTS = 900;
     private final int SIZE_DOT = 10;
     private final int RAND_POS = 29;
     private final int DELAY = 140;
 
-    private final int x[] = new int[DOTS];
-    private final int y[] = new int[DOTS];
+    private final int x[] = new int[MAX_DOTS];
+    private final int y[] = new int[MAX_DOTS];
 
     private int dots;
     private int apple_x;
@@ -87,10 +87,111 @@ public class Board extends JPanel implements ActionListener {
             }
             Toolkit.getDefaultToolkit().sync();
         } else {
-            
+            gameOver(g);
         }
 
 
+    }
+    private void gameOver(Graphics g) {
+        String msg = "Game Over";
+        Font small = new Font("Helvetica",Font.BOLD,14);
+        FontMetrics metr = getFontMetrics(small);
+
+        g.setColor(Color.white);
+        g.setFont(small);
+        g.drawString(msg,(BOARD_WIDTH-metr.stringWidth(msg))/2,BOARD_HEIGHT/2);
+    }
+
+    private void checkApple() {
+        if((x[0]==apple_x) && (y[0] == apple_y)) {
+            dots++;
+            locateApple();
+        }
+    }
+
+    private void move() {
+        for (int z= dots;z>0;z--) {
+            x[z] = x[(z-1)];
+            y[z] = y[(z-1)];
+        }
+
+        if (leftDirection) {
+            x[0] -= SIZE_DOT;
+        } else if(rightDirection) {
+            x[0] += SIZE_DOT;
+        } else if(upDirection) {
+            y[0] -= SIZE_DOT;
+        } else if(downDirection){
+            y[0] += SIZE_DOT;
+        }
+    }
+
+    private void checkCollision() {
+        
+        for (int z = dots;z>0;z--) {
+            if((z>4) &&(x[0]==x[z]) && (y[0] == y[z])) {
+                inGame = false;
+            }
+        }
+        if (y[0] >= BOARD_HEIGHT) {
+            inGame = false;
+        } else if(y[0]<0) {
+            inGame = false;
+        } else if (x[0]>=BOARD_WIDTH) {
+            inGame = false;
+        } else if(x[0] < 0) {
+            inGame = false;
+        }
+        if(!inGame) {
+            timer.stop();
+        }
+    }
+
+    private void locateApple() {
+        int r = (int) (Math.random()* RAND_POS);
+        apple_x = ((r*SIZE_DOT));
+
+        r= (int) (Math.random()*RAND_POS);
+        apple_y = ((r*SIZE_DOT));
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+        if(inGame) {
+            checkApple();
+            checkCollision();
+            move();
+        }
+
+        repaint();
+    }
+
+    private class TAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e){
+            int key = e.getKeyCode();
+
+            if((key == KeyEvent.VK_LEFT)&&(!rightDirection)) {
+                leftDirection = true;
+                upDirection = false;
+                downDirection = false;
+            }
+            if ((key==KeyEvent.VK_RIGHT)&&(!leftDirection)) {
+                rightDirection = true;
+                upDirection = false;
+                downDirection = false;
+            }
+            if((key == KeyEvent.VK_UP)&&(!downDirection)){
+                upDirection  = true;
+                rightDirection = false;
+                leftDirection = false;
+            }
+            if((key==KeyEvent.VK_DOWN)&&(!upDirection)) {
+                downDirection = true;
+                rightDirection = false;
+                leftDirection = false;
+            }
+        }
     }
 
 }
